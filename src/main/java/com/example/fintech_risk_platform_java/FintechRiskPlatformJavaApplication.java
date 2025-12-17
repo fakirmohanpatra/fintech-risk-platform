6cf97679f41252b86fb982c1eeb98cf47b42283a
fmp-kongs <fakirmohan.patra@kongsbergdigital.com>
2025-12-17 08:46:09 +0530

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/example/fintech_risk_platform_java/FintechRiskPlatformJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintech_risk_platform_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechRiskPlatformJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechRiskPlatformJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fintech_risk_platform_java/FintechRiskPlatformJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fintech_risk_platform_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechRiskPlatformJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
